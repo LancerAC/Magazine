56ac5b704757a2d2a9dd7325339df5735425b5ff
LancerAC <ilya.pashchenko.04@inbox.ru>
2024-01-16 16:49:51 +0300

    create Entity Post

---
+++ src/main/java/com/example/magazine/Entity/Post.java
@@ -0,0 +1,25 @@
+package com.example.magazine.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title, anons, full_text;
+    private int views;
+
+}
