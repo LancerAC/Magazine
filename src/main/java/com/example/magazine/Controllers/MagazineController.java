bef203c8e079b7c4c91ee8b97959623a4cb2ce21
LancerAC <ilya.pashchenko.04@inbox.ru>
2024-01-16 17:57:56 +0300

    add method for add Blog

--- src/main/java/com/example/magazine/Controllers/MagazineController.java
+++ src/main/java/com/example/magazine/Controllers/MagazineController.java
@@ -1,14 +1,29 @@
 package com.example.magazine.Controllers;
 
+import com.example.magazine.Entity.Post;
+import com.example.magazine.Repository.PostRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class MagazineController {
+
+    private final PostRepository postRepository;
+
     @GetMapping("/magazine")
     public String getMagazine(Model model){
-
+        Iterable<Post> posts = postRepository.findAll();
+        model.addAttribute("postList", posts);
         return "magazine";
+    }
+    @GetMapping("/magazine/add")
+    public String addMagazine(Model model){
+
+        return "magazine-add";
     }
 }
